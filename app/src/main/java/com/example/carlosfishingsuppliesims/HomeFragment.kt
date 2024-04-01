47edaeab4cdc71d29ed0099a588e2c740d87ada2
KagoShinji <johnjosfir312@gmail.com>
2024-04-02 04:07:43 +0700

    seventh commit. clickable notification to fragment

--- app/src/main/java/com/example/carlosfishingsuppliesims/HomeFragment.kt
+++ app/src/main/java/com/example/carlosfishingsuppliesims/HomeFragment.kt
@@ -17,6 +17,7 @@ import com.google.firebase.database.ValueEventListener
 class HomeFragment : Fragment() {
 
     private lateinit var allProductsTextView: TextView
+    private lateinit var lowProductsTextView: TextView
     private lateinit var productsRef: DatabaseReference
 
     override fun onCreateView(
@@ -27,16 +28,29 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
         allProductsTextView = view.findViewById(R.id.allProducts)
+        lowProductsTextView = view.findViewById(R.id.lowProducts)
         productsRef = FirebaseDatabase.getInstance().getReference("products")
 
-        // Listen for changes in the database and update the TextView accordingly
+        // Listen for changes in the database and update the TextViews accordingly
         productsRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get the count of products
+                // Get the count of all products
                 val numberOfProducts = dataSnapshot.childrenCount
 
-                // Update the TextView
+                // Update the allProductsTextView
                 allProductsTextView.text = numberOfProducts.toString()
+
+                // Count products with less than 5 quantity
+                var lowProductsCount = 0
+                for (productSnapshot in dataSnapshot.children) {
+                    val quantity = productSnapshot.child("quantity").getValue(Long::class.java) ?: 0
+                    if (quantity < 5) {
+                        lowProductsCount++
+                    }
+                }
+
+                // Update the lowProductsTextView
+                lowProductsTextView.text = lowProductsCount.toString()
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -44,8 +58,15 @@ class HomeFragment : Fragment() {
             }
         })
 
-        val cardView = view.findViewById<CardView>(R.id.productsNotification)
-        cardView.setOnClickListener {
+        // Navigate to ProductsFragment when lowStockNotification CardView is clicked
+        val lowStockNotificationCardView = view.findViewById<CardView>(R.id.lowStockNotification)
+        lowStockNotificationCardView.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_productsFragment)
+        }
+
+        // Navigate to ProductsFragment when productsNotification CardView is clicked
+        val productsNotificationCardView = view.findViewById<CardView>(R.id.productsNotification)
+        productsNotificationCardView.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_productsFragment)
         }
 
