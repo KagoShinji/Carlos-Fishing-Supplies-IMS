0ffeed235c73af969dcc0a3e78f50f80691213fa
KagoShinji <johnjosfir312@gmail.com>
2024-04-02 03:09:07 +0700

    seventh commit. clickable notification to fragment

--- app/src/main/java/com/example/carlosfishingsuppliesims/HomeFragment.kt
+++ app/src/main/java/com/example/carlosfishingsuppliesims/HomeFragment.kt
@@ -1,14 +1,23 @@
 package com.example.carlosfishingsuppliesims
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.cardview.widget.CardView
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
+
+    private lateinit var allProductsTextView: TextView
+    private lateinit var productsRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +26,24 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        allProductsTextView = view.findViewById(R.id.allProducts)
+        productsRef = FirebaseDatabase.getInstance().getReference("products")
+
+        // Listen for changes in the database and update the TextView accordingly
+        productsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get the count of products
+                val numberOfProducts = dataSnapshot.childrenCount
+
+                // Update the TextView
+                allProductsTextView.text = numberOfProducts.toString()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle database error
+            }
+        })
+
         val cardView = view.findViewById<CardView>(R.id.productsNotification)
         cardView.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_productsFragment)
@@ -24,5 +51,4 @@ class HomeFragment : Fragment() {
 
         return view
     }
-
 }
